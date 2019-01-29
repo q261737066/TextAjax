adc66b5b63b5e0c758e642f2925da553bd91abdb
q2617 <q2617@192.168.3.6>
2019-01-29 13:53:14 +0900

    ppppp

--- TextAjax/src/com/text/bean/Text.java
+++ TextAjax/src/com/text/bean/Text.java
@@ -1,5 +1,84 @@
 package com.text.bean;
 
 public class Text {
+ private int  id;
+ private String name;
+ private String birthday;
+ private int  age;
+/**
+ * @return id
+ */
+public int getId() {
+	return id;
+}
+/**
+ * @param id セットする id
+ */
+public void setId(int id) {
+	this.id = id;
+}
+/**
+ * @return name
+ */
+public String getName() {
+	return name;
+}
+/**
+ * @param name セットする name
+ */
+public void setName(String name) {
+	this.name = name;
+}
+/**
+ * @return birthday
+ */
+public String getBirthday() {
+	return birthday;
+}
+/**
+ * @param birthday セットする birthday
+ */
+public void setBirthday(String birthday) {
+	this.birthday = birthday;
+}
+/**
+ * @return age
+ */
+public int getAge() {
+	return age;
+}
+/**
+ * @param age セットする age
+ */
+public void setAge(int age) {
+	this.age = age;
+}
+/* (非 Javadoc)
+ * @see java.lang.Object#toString()
+ */
+@Override
+public String toString() {
+	return "Text [id=" + id + ", name=" + name + ", birthday=" + birthday + ", age=" + age + "]";
+}
+/**
+ * @param id
+ * @param name
+ * @param birthday
+ * @param age
+ */
+public Text(int id, String name, String birthday, int age) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.birthday = birthday;
+	this.age = age;
+}
+/**
+ *
+ */
+public Text() {
+	super();
+	// TODO 自動生成されたコンストラクター・スタブ
+}
 
 }
--- TextAjax/src/com/text/controller/TextController.java
+++ TextAjax/src/com/text/controller/TextController.java
@@ -1,8 +1,16 @@
 package com.text.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Text;
+
 //@Controller
 public class TextController {
 	//@RequestMapping();
   public String queryAll() {
+
+	  List<Text> textList=new ArrayList<Text>();
 	return null;
 
   }
